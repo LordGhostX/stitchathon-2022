a168c26ddbee74f4c1ebdd3224036b3d2957c5aa
LordGhostX <solomonghost9@gmail.com>
2022-02-19 12:00:51 +0100

    added register page

--- app.py
+++ app.py
@@ -10,5 +10,15 @@ def index():
     return render_template("index.html")
 
 
+@app.route("/auth/register/")
+def register():
+    return render_template("register.html")
+
+
+@app.route("/auth/login/")
+def login():
+    return render_template("login.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
